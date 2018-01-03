e8a008058794a5381da8b4346bab94078375a412
Ting <first971337@gmail.com>
2018-01-03 15:02:29 +0800

    Initial commit

---
+++ app/src/main/java/com/first80118/d2018010301/MainActivity.java
@@ -0,0 +1,20 @@
+package com.first80118.d2018010301;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView) findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient());
+
+        wv.loadUrl("http://www.hinet.net");
+    }
+}
